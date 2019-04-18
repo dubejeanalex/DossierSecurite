6b012d17b7594e73db76ef3a6f2791072174185d
dubejeanalex <alexandre.dubejean@live.ca>
2019-04-18 16:43:48 -0400

    C'est un bon debut

--- BruteForce.py
+++ BruteForce.py
@@ -1,27 +1,37 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz';
-motDePasse= 'cis'
+motDePasse= 'aaaaa'
 motdePasseConfirmation=""
 lettre1=''
 lettre2=''
 lettre3=''
+lettre4=''
+lettre5=''
 compteur=0
 
-for i in range(26):
+while motdePasseConfirmation != motDePasse:
+    for i in range(26):
 
-    lettre1=alphabet[i]
+        lettre1=alphabet[i]
 
-    for j in range(26):
-        lettre2=alphabet[j]
-        for k in range(26):
-            lettre3=alphabet[k]
+        for j in range(26):
+            lettre2=alphabet[j]
+            for k in range(26):
+                lettre3=alphabet[k]
+                for l in range(26):
+                    lettre4=alphabet[l]
+                    for m in range(26):
+                        lettre5=alphabet[m]
 
-            motdePasseConfirmation =lettre1+lettre2+lettre3
 
 
-            if motDePasse== motdePasseConfirmation:
-                print("wouhou !!!!")
+                        motdePasseConfirmation =lettre1+lettre2+lettre3+lettre4+lettre5
+                        print(motdePasseConfirmation)
 
-            print (motDePasse)
-            print (motdePasseConfirmation)
+                        if motDePasse== motdePasseConfirmation:
+                             print("wouhou !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11111")
+                             break
+
+
+    print (motdePasseConfirmation)
 
 
